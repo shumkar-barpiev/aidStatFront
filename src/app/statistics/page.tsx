6320a1553d0b03c5c4def2a1d9937151ebfc6d03
Almaz Balbaev <kujitengu@gmail.com>
2025-04-08 15:41:21 +0600

    feat: added tabs logic

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,14 +1,8 @@
 "use client";
-import dynamic from "next/dynamic";
 import { Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Main from "@/components/statistics/charts/Main";
 import { containerMargins, containerWidths } from "@/utils/constants";
-import ContractsMap from "@/components/maps/ContractsMap.tsx";
-
-const ProjectsMap = dynamic(() => import("@/components/maps/ProjectsMap"), {
-  ssr: false,
-});
 
 export default function StatisticsPage() {
   const [isClient, setIsClient] = useState(false);
@@ -21,8 +15,6 @@ export default function StatisticsPage() {
 
   return (
     <Box sx={{ width: containerWidths, mx: containerMargins, p: 2, minHeight: "100vh" }}>
-      <ProjectsMap />
-      <ContractsMap />
       <Main />
     </Box>
   );
