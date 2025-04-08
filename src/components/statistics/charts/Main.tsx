2126359cc5e5c3355c858ff235a922acd1e118b1
Almaz Balbaev <kujitengu@gmail.com>
2025-04-08 15:41:54 +0600

    feat: added tabs logic maps components moved to Main

--- src/components/statistics/charts/Main.tsx
+++ src/components/statistics/charts/Main.tsx
@@ -1,28 +1,59 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Box, Typography, Divider, Grid } from "@mui/material";
+import { Box, Typography, Divider, Grid, Tab, Tabs } from "@mui/material";
 import CardComponent from "@/components/statistics/charts/ChartCard";
 import { useStatisticsChartsViewModel } from "@/viewmodels/statistics/charts/useStatisticChartsViewModel";
+import ContractsMap from "@/components/maps/ContractsMap.tsx";
+import dynamic from "next/dynamic";
+import CustomTabPanel from "@/components/tabs/CustomTabPanel.tsx";
+
+const ProjectsMap = dynamic(() => import("@/components/maps/ProjectsMap"), {
+  ssr: false,
+});
+
+const a11yProps = (index: number) => {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+};
 
 export default function Main() {
+  const [tab, setTab] = useState<number>(0);
   const { t } = useTranslation();
   const { cardsData } = useStatisticsChartsViewModel();
 
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
+  };
+
   return (
     <Box sx={{ width: 1 }}>
-      <Typography variant="h4" fontWeight="bold" sx={{ my: 3, textAlign: "left" }}>
-        {t("statisticsTitle")}
-      </Typography>
-      <Divider sx={{ mb: 6, borderColor: "darkblue", borderBottomWidth: 2 }} />
-      <Grid container spacing={3}>
-        {cardsData.map((card) => (
-          <Grid item xs={12} sm={12} lg={6} key={card.id}>
-            <CardComponent card={card} />
-          </Grid>
-        ))}
-      </Grid>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={tab} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Тепловая карта проектов" {...a11yProps(0)} />
+          <Tab label="Карта контрактов" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={tab} index={0}>
+        <ProjectsMap />
+        <Typography variant="h4" fontWeight="bold" sx={{ my: 3, textAlign: "left" }}>
+          {t("statisticsTitle")}
+        </Typography>
+        <Divider sx={{ mb: 6, borderColor: "darkblue", borderBottomWidth: 2 }} />
+        <Grid container spacing={3}>
+          {cardsData.map((card) => (
+            <Grid item xs={12} sm={12} lg={6} key={card.id}>
+              <CardComponent card={card} />
+            </Grid>
+          ))}
+        </Grid>
+      </CustomTabPanel>
+      <CustomTabPanel value={tab} index={1}>
+        <ContractsMap />
+      </CustomTabPanel>
     </Box>
   );
 }
