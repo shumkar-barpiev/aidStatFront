8c4019da1530423a7fadb50d2e38c42e2552e08d
Shumkarbek Barpyev <74590123+shumkar-barpiev@users.noreply.github.com>
2025-04-14 11:27:43 +0600

    Исправьте show page проекта для sm и xs экранов (#26)

--- src/components/projects/show/ProjectTimeLine.tsx
+++ src/components/projects/show/ProjectTimeLine.tsx
@@ -1,91 +1,96 @@
 import * as React from "react";
+
 import Timeline from "@mui/lab/Timeline";
 import TimelineDot from "@mui/lab/TimelineDot";
+import { Box, Typography } from "@mui/material";
 import TimelineItem from "@mui/lab/TimelineItem";
-import Typography from "@mui/material/Typography";
 import TimelineContent from "@mui/lab/TimelineContent";
+import EventNoteIcon from "@mui/icons-material/EventNote";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import TimelineConnector from "@mui/lab/TimelineConnector";
+import { TProjectModel } from "@/models/project/ProjectModel";
 import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
 
-export default function ProjectTimeLine() {
+export default function ProjectTimeLine({ project }: { project: TProjectModel | null }) {
+  const FormatDateWithTime = (dateString: string) => {
+    const date = new Date(dateString);
+
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+
+    return `${day}.${month}.${year}`;
+  };
+
   return (
-    <Timeline position="alternate">
-      <TimelineItem>
-        <TimelineOppositeContent sx={{ m: "auto 0" }} align="right" variant="body2" color="text.secondary">
-          01.01.2021
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary"></TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ m: "auto 0" }}>
-          <Typography variant="h6" component="span">
-            Инициация проекта
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent sx={{ m: "auto 0" }} variant="body2" color="text.secondary">
-          01.02.2021
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary"></TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ m: "auto 0" }}>
-          <Typography variant="h6" component="span">
-            Строительный этап 1
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent sx={{ m: "auto 0" }} variant="body2" color="text.secondary">
-          01.04.2021
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary" variant="outlined"></TimelineDot>
-          <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
-        </TimelineSeparator>
-        <TimelineContent sx={{ m: "auto 0" }}>
-          <Typography variant="h6" component="span">
-            Ключевой этап завершен
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent sx={{ m: "auto 0" }} variant="body2" color="text.secondary">
-          01.05.2021
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
-          <TimelineDot color="secondary"></TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ m: "auto 0" }}>
-          <Typography variant="h6" component="span">
-            Завершение строительства
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent sx={{ m: "auto 0" }} variant="body2" color="text.secondary">
-          01.08.2021
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary"></TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ m: "auto 0" }}>
-          <Typography variant="h6" component="span">
-            Подведение итогов
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-    </Timeline>
+    <Box>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 1, mb: 1 }}>
+        <EventNoteIcon color="primary" fontSize="large" />
+        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+          Хронология проекта
+        </Typography>
+      </Box>
+      <Timeline position="alternate">
+        {project?.timeLine?.startDate && (
+          <TimelineItem>
+            <TimelineOppositeContent sx={{ m: "auto 0" }} align="right" variant="body2" color="text.secondary">
+              {project?.timeLine?.startDate && FormatDateWithTime(project?.timeLine?.startDate)}
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot color="primary"></TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ m: "auto 0" }}>
+              <Typography variant="h6" component="span">
+                Дата начала
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+        )}
+
+        {project?.timeLine?.events &&
+          project.timeLine.events.map((event: Record<string, any>, index: number) => {
+            return (
+              event.name &&
+              event.eventDate && (
+                <TimelineItem key={index}>
+                  <TimelineOppositeContent sx={{ m: "auto 0" }} variant="body2" color="text.secondary">
+                    {FormatDateWithTime(event.eventDate)}
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineConnector />
+                    <TimelineDot color="primary"></TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent sx={{ m: "auto 0" }}>
+                    <Typography variant="h6" component="span">
+                      {event.name}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+              )
+            );
+          })}
+
+        {project?.timeLine?.endDate && (
+          <TimelineItem>
+            <TimelineOppositeContent sx={{ m: "auto 0" }} align="right" variant="body2" color="text.secondary">
+              {project?.timeLine?.endDate && FormatDateWithTime(project?.timeLine?.endDate)}
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot color="primary"></TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ m: "auto 0" }}>
+              <Typography variant="h6" component="span">
+                Дата окончания
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+        )}
+      </Timeline>
+    </Box>
   );
 }
