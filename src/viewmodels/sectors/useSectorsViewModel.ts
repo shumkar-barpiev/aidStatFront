e746d4e380ece62f820db0d1ecc5b3134c59d45c
Shumkarbek Barpyev <74590123+shumkar-barpiev@users.noreply.github.com>
2025-04-15 16:08:54 +0600

    Реализуйте логику зависимых секторов и групп секторов в фильтре (#32)

--- src/viewmodels/sectors/useSectorsViewModel.ts
+++ src/viewmodels/sectors/useSectorsViewModel.ts
@@ -6,21 +6,53 @@ import { useAidStatSectorsStore } from "@/stores/dictionaries/aidstat-sectors";
 interface SectorOption {
   id: number;
   name: string;
-  projectCount: number;
   type: string;
+  projectCount: number;
+  children: Record<string, any>[] | null;
 }
 
 export const useSectorsViewModel = () => {
   const sectorsStore = useAidStatSectorsStore();
   const [sectors, setSectors] = useState<SectorOption[]>([]);
+  const [sectorsGroup, setSectorsGroup] = useState<SectorOption[]>([]);
+  const [allChildrenMap, setAllChildrenMap] = useState<Record<number, SectorOption[]>>({});
+
+  const handleSectorGroupChange = (parentIds: number[]) => {
+    if (parentIds.length > 0) {
+      const filteredParents = sectorsGroup.filter((group) => parentIds.includes(group.id));
+      let children: Record<string, any>[] = [];
+      filteredParents.map((parent) => {
+        if (parent.children?.length) {
+          parent.children.map((child) => children.push(child));
+        }
+      });
+
+      setSectors(children as SectorOption[]);
+    } else {
+      const allChildren = Object.values(allChildrenMap).flat();
+      setSectors(allChildren);
+    }
+  };
 
   useEffect(() => {
     sectorsStore.fetchItems((data: SectorOption[]) => {
-      setSectors(data);
+      setSectorsGroup(data);
+      const children = data.flatMap((item) => item.children || []);
+      setSectors(children as SectorOption[]);
+
+      const childrenMap: Record<number, SectorOption[]> = {};
+      data.forEach((item) => {
+        if (item.children && item.children.length > 0) {
+          childrenMap[item.id] = item.children as SectorOption[];
+        }
+      });
+      setAllChildrenMap(childrenMap);
     });
   }, []);
 
   return {
     sectors,
+    sectorsGroup,
+    handleSectorGroupChange,
   };
 };
