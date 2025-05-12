8f0e09d10efc33f9af5229b658caae454311d283
Almaz <red.rokku@gmail.com>
2025-05-12 09:19:51 +0600

    feat: contracts search in tables comp logic

--- src/components/statistics/components/search/ContractsSearchField.tsx
+++ src/components/statistics/components/search/ContractsSearchField.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { InputAdornment } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { StyledTextField } from "@/components/other/StyledTextField.tsx";
-import { useContractsStore } from "@/stores/contracts/contracts.ts";
+import { StyledTextField } from "@/components/other/StyledTextField";
+import { useContractsStore } from "@/stores/contracts/contracts";
+import { useTranslation } from "react-i18next";
 
 interface Props {
   handleSetFilter: (searchString: string) => void;
@@ -12,14 +13,22 @@ interface Props {
 
 const ContractsSearchField: React.FC<Props> = ({ handleSetFilter }) => {
   const filters = useContractsStore((state) => state.filters);
+  const [inputValue, setInputValue] = useState<string>(filters?.searchString ?? "");
+  const { t } = useTranslation();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+    handleSetFilter(value);
+  };
 
   return (
     <StyledTextField
       fullWidth
-      // placeholder={t("search-placeholder")}
-      // variant="outlined"
-      // onChange={onChange}
-      // value={inputValue}
+      placeholder={t("search-placeholder")}
+      variant="outlined"
+      onChange={onChange}
+      value={inputValue}
       sx={{ width: 1, my: 1 }}
       InputProps={{
         startAdornment: (
--- src/viewmodels/contracts/useTableContractsViewModel.ts
+++ src/viewmodels/contracts/useTableContractsViewModel.ts
@@ -1,20 +1,22 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { useContractsStore } from "@/stores/contracts/contracts.ts";
-import { TContractModelForMap } from "@/models/contracts/ContractModel.ts";
+import { useRef, useEffect, ChangeEvent } from "react";
+import { useContractsStore } from "@/stores/contracts/contracts";
+import { debounce } from "@/utils/utils";
 
 export const useTableContractsViewModel = () => {
   const { fetchContractsForTable, setFilters, filters } = useContractsStore();
+
+  const debouncedFetch = useRef(debounce(() => fetchContractsForTable(), 500)).current;
 
   const handleChangePage = (event: ChangeEvent<unknown>, page: number) => {
     setFilters({ page });
   };
 
   const handleSetFilter = (searchString: string) => {
-    setFilters({ searchString });
+    setFilters({ searchString, page: 1 });
   };
 
   useEffect(() => {
-    fetchContractsForTable();
+    debouncedFetch();
   }, [filters.page, filters.searchString, filters.status]);
 
   return {
